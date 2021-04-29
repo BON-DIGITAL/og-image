d8ac78f5ac16c5dcc3ed70cca4d12ecf2cc3f48f
James Hilton-Barber <jameshiltonbarber@gmail.com>
2021-04-29 11:42:18 +0200

    Revert "og-image-generator"
    This reverts commit 4a897433f51d601d35d9203b5a42c055e2943ae9.

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -104,7 +104,7 @@ function getCss(theme: string, fontSize: string) {
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize } = parsedReq;
+    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
@@ -116,6 +116,12 @@ export function getHtml(parsedReq: ParsedRequest) {
     <body>
         <div>
             <div class="spacer">
+            <div class="logo-wrapper">
+                ${images.map((img, i) =>
+                    getPlusSign(i) + getImage(img, widths[i], heights[i])
+                ).join('')}
+            </div>
+            <div class="spacer">
             <div class="heading">${emojify(
                 md ? marked(text) : sanitizeHtml(text)
             )}
@@ -125,16 +131,16 @@ export function getHtml(parsedReq: ParsedRequest) {
 </html>`;
 }
 
-// function getImage(src: string, width ='auto', height = '225') {
-//     return `<img
-//         class="logo"
-//         alt="Generated Image"
-//         src="${sanitizeHtml(src)}"
-//         width="${sanitizeHtml(width)}"
-//         height="${sanitizeHtml(height)}"
-//     />`
-// }
+function getImage(src: string, width ='auto', height = '225') {
+    return `<img
+        class="logo"
+        alt="Generated Image"
+        src="${sanitizeHtml(src)}"
+        width="${sanitizeHtml(width)}"
+        height="${sanitizeHtml(height)}"
+    />`
+}
 
-// function getPlusSign(i: number) {
-//     return i === 0 ? '' : '<div class="plus">+</div>';
-// }
+function getPlusSign(i: number) {
+    return i === 0 ? '' : '<div class="plus">+</div>';
+}
